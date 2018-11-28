f7ca6964452fed2d847f3fe928eebad402e8bba5
Jake <jakemiller13@hotmail.com>
2018-11-28 06:58:37 -0500

    Trained model, added Flatten
    Working as of 11/28.
    -Added Flatten() class to flatten tensor before fully connected layer
    -Trained and validated model
    -Getting ~90% accuracy
    To do:
    *Put training/evaluation/etc. in functions for clarity
    *Try dropout to see if you can improve accuracy

--- Fashion-MNIST Classification - JM1.py
+++ Fashion-MNIST Classification - JM1.py
@@ -19,6 +19,7 @@ import torch
 import torch.nn as nn
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 import matplotlib.pylab as plt
 import numpy as np
 from collections import OrderedDict
@@ -72,6 +73,13 @@ def plot_parameters(weights, title):
     plt.suptitle(title)
     plt.show()
 
+class Flatten(nn.Module):
+    '''
+    Used in Sequential container to flatten tensor
+    '''
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
 # Load data
 train_dataset = dsets.FashionMNIST(root = './JM1',
                                    train = True,
@@ -82,16 +90,65 @@ validation_dataset = dsets.FashionMNIST(root = './JM1',
                                         download = True,
                                         transform = transforms.ToTensor())
 
+# Create dataset loaders
+train_loader = DataLoader(train_dataset, batch_size = 100)
+validation_loader = DataLoader(validation_dataset, batch_size = 5000)
+
 model = nn.Sequential(OrderedDict([
         ('conv1',       nn.Conv2d(in_channels = 1, out_channels = 16,
-                                  kernel_size = 5, stride = 1, padding = 2)),
+                                  kernel_size = 5, padding = 2)),
         ('relu1',       nn.ReLU()),
         ('maxpool1',    nn.MaxPool2d(kernel_size = 2)),
         ('conv2',       nn.Conv2d(in_channels = 16, out_channels = 32,
                                   kernel_size = 5, stride = 1, padding = 2)),
         ('relu2',       nn.ReLU()),
         ('maxpool2',    nn.MaxPool2d(kernel_size = 2)),
+        ('flatten',     Flatten()),
         ('dense1',      nn.Linear(32 * 7 * 7, 10))])) # multiplication?
+
+# Training/model constants
+learning_rate = 0.1
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+epochs = 10
+
+# Create lists for accuracy dictionary
+loss_list = []
+accuracy_list = []
+
+# Train & evaluate model
+for epoch in range(epochs):
+    print('Evaluating epoch: ' + str(epoch + 1) + '/' + str(epochs))
+    for x, y in train_loader:
+        optimizer.zero_grad()
+        output = model(x)
+        loss = criterion(output, y)
+        loss.backward()
+        optimizer.step()
+
+# Check accuracy when complete
+correct = 0      
+for x, y in validation_loader:
+    print('Validating accuracy...')
+    output = model(x)
+    _, y_hat = torch.max(output.data, 1)
+    correct += (y_hat == y).sum().item()
+accuracy = correct / len(validation_dataset)
+print('Accuracy on 10000 images: ' + str(accuracy * 100) + '%')
+
+# Check first 5 misclassified
+count = 0
+for x, y in DataLoader(dataset = validation_dataset, batch_size=1):
+    output = model(x)
+    _, y_hat=torch.max(output, 1)
+    if y_hat != y:
+        show_image((x,y))
+        plt.show()
+        print("Classified as: " + index_to_class(int(y_hat)) + 
+              ' (' + str(y_hat.item()) + ')')
+        count += 1
+    if count >= 5:
+        break
 
 # Testing
 rand_num = np.random.randint(0,10)

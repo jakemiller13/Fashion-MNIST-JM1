396888f5f2a9dc0f793d0deb9f64ad9218a12d5d
Jacob Miller <jmiller@xlrn.com>
2018-11-28 16:43:13 -0500

    Put everything into functions
    Drastically changed flow of program. Put everything into functions for easier testing/viewing

--- Fashion-MNIST Classification - JM1.py
+++ Fashion-MNIST Classification - JM1.py
@@ -24,7 +24,9 @@ import matplotlib.pylab as plt
 import numpy as np
 from collections import OrderedDict
 
-# Utility/helper functions
+############################
+# UTILITY/HELPER FUNCTIONS #
+############################
 def show_image(image):
     '''
     Displays 28x28 pixel image with correct classification
@@ -80,20 +82,79 @@ class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
 
-# Load data
-train_dataset = dsets.FashionMNIST(root = './JM1',
-                                   train = True,
-                                   download = True,
-                                   transform = transforms.ToTensor())
-validation_dataset = dsets.FashionMNIST(root = './JM1',
-                                        train = False,
-                                        download = True,
-                                        transform = transforms.ToTensor())
+#####################
+# Functions for CNN #
+#####################
+def create_datasets():
+    '''
+    Creates FashionMNIST dataset
+    Creates "train_dataset" and "validation_dataset"
+    Currently saves to "./JM1"
+    '''
+    train_dataset = dsets.FashionMNIST(root = './JM1',
+                                       train = True,
+                                       download = True,
+                                       transform = transforms.ToTensor())
+    validation_dataset = dsets.FashionMNIST(root = './JM1',
+                                            train = False,
+                                            download = True,
+                                            transform = transforms.ToTensor())
+    return train_dataset, validation_dataset
 
-# Create dataset loaders
-train_loader = DataLoader(train_dataset, batch_size = 100)
-validation_loader = DataLoader(validation_dataset, batch_size = 5000)
+def create_loaders(train_dataset, validation_dataset):
+    '''
+    Creates "train_loader" and "validation_loader" from respective datasets
+    '''
+    train_loader = DataLoader(train_dataset, batch_size = 100)
+    validation_loader = DataLoader(validation_dataset, batch_size = 5000)
+    return train_loader, validation_loader
 
+def train_model(model, epochs, train_loader, optimizer, criterion):
+    '''
+    Trains "model" for "n_epochs" using "train_loader, optimizer, criterion"
+    '''
+    for epoch in range(epochs):
+        print('Evaluating epoch: ' + str(epoch + 1) + '/' + str(epochs))
+        for x, y in train_loader:
+            optimizer.zero_grad()
+            output = model(x)
+            loss = criterion(output, y)
+            loss.backward()
+            optimizer.step()
+
+def check_accuracy(model, validation_loader, validation_dataset):
+    '''
+    Checks accuracy of "model" using "validation_loader, validation_dataset"
+    '''
+    correct = 0      
+    for x, y in validation_loader:
+        print('Validating accuracy...')
+        output = model(x)
+        _, y_hat = torch.max(output.data, 1)
+        correct += (y_hat == y).sum().item()
+    accuracy = correct / len(validation_dataset)
+    print('Accuracy on 10000 images: ' + str(accuracy * 100) + '%')
+
+def check_misclassified(model, validation_dataset, n_misclassified):
+    '''
+    Checks first n_misclassified
+    '''
+    count = 0
+    for x, y in DataLoader(dataset = validation_dataset, batch_size=1):
+        output = model(x)
+        _, y_hat=torch.max(output, 1)
+        if y_hat != y:
+            show_image((x,y))
+            plt.show()
+            print("Classified as: " + index_to_class(int(y_hat)) + 
+                  ' (' + str(y_hat.item()) + ')')
+            count += 1
+        if count >= n_misclassified:
+            break
+
+#############
+# CNN MODEL #
+#############
 model = nn.Sequential(OrderedDict([
         ('conv1',       nn.Conv2d(in_channels = 1, out_channels = 16,
                                   kernel_size = 5, padding = 2)),
@@ -106,52 +167,34 @@ model = nn.Sequential(OrderedDict([
         ('flatten',     Flatten()),
         ('dense1',      nn.Linear(32 * 7 * 7, 10))])) # multiplication?
 
+###########
+# TESTING #
+###########
+rand_num = np.random.randint(0,10)
+
 # Training/model constants
 learning_rate = 0.1
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
 epochs = 10
 
-# Create lists for accuracy dictionary
+# Create lists for accuracy dictionary - to be used in future for plotting
 loss_list = []
 accuracy_list = []
 
-# Train & evaluate model
-for epoch in range(epochs):
-    print('Evaluating epoch: ' + str(epoch + 1) + '/' + str(epochs))
-    for x, y in train_loader:
-        optimizer.zero_grad()
-        output = model(x)
-        loss = criterion(output, y)
-        loss.backward()
-        optimizer.step()
+#############
+# RUN MODEL #
+#############
+train_dataset, validation_dataset = create_datasets()
+train_loader, validation_loader = create_loaders(train_dataset,
+                                                 validation_dataset)
+train_model(model, epochs, train_loader, optimizer, criterion)
+check_accuracy(model, validation_loader, validation_dataset)
+check_misclassified(model, validation_dataset, n_misclassified = 5)
 
-# Check accuracy when complete
-correct = 0      
-for x, y in validation_loader:
-    print('Validating accuracy...')
-    output = model(x)
-    _, y_hat = torch.max(output.data, 1)
-    correct += (y_hat == y).sum().item()
-accuracy = correct / len(validation_dataset)
-print('Accuracy on 10000 images: ' + str(accuracy * 100) + '%')
-
-# Check first 5 misclassified
-count = 0
-for x, y in DataLoader(dataset = validation_dataset, batch_size=1):
-    output = model(x)
-    _, y_hat=torch.max(output, 1)
-    if y_hat != y:
-        show_image((x,y))
-        plt.show()
-        print("Classified as: " + index_to_class(int(y_hat)) + 
-              ' (' + str(y_hat.item()) + ')')
-        count += 1
-    if count >= 5:
-        break
-
-# Testing
-rand_num = np.random.randint(0,10)
+##########################
+# A COUPLE OF FUN CHECKS #
+##########################
 
 show_image(train_dataset[rand_num])
 plot_parameters(model.state_dict()['conv1.weight'],
